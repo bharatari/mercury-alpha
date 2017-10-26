a3a734418371aa5a85bde4be58acba8086a1650c
Bharat Arimilli <bbarimilli@outlook.com>
2017-10-26 11:27:17 -0500

    Support multiple crontabs

--- src/task.js
+++ src/task.js
@@ -2,19 +2,30 @@ import cron from 'cron';
 import record from './record';
 import social from './social';
 import config from './config';
+import _ from 'lodash';
 
 const CronJob = cron.CronJob;
 
 export function recordTask(crontab) {
-  const job = new CronJob(crontab, () => {
-    console.log('Recorder Running');
+  if (crontab) {
+    if (_.isArray(crontab)) {
+      for (let i = 0; i < crontab.length; i++) {
+        const job = new CronJob(crontab[i], () => {
+          console.log('Recorder Running');
 
-    record(() => {
-      console.log('Recorder Finished');
-    });
-  });
+          record(() => {
+            console.log('Recorder Finished');
+          });
+        });
 
-  job.start();
+        job.start();
+      }
+    } else {
+      throw new Error('Crontab must be an array of valid cron tabs.')
+    }
+  } else {
+    throw new Error('Crontab must be an array of valid cron tabs.')
+  }  
 }
 
 export function dailySocialTask() {
